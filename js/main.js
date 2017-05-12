7004e83620d0f5f7c28035353b3ea10ab32d3a9a
euonymblog <antoniamochan@Antonias-MacBook-Air.local>
2017-05-12 20:46:32 +0800

    Improved js menu

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
   $( ".menu" ).hide();
   $( ".responsive-nav" ).click(function() {
   $( ".menu" ).slideToggle( "slow", function() {
-  $( ".responsive-nav" ).hide();
-  $( ".cross" ).show();
+  // $( ".responsive-nav" ).hide();
+  // $( ".cross" ).show();
   });
   });
 
   $( ".cross" ).click(function() {
   $( ".menu" ).slideToggle( "slow", function() {
-  $( ".cross" ).hide();
+  // $( ".cross" ).hide();
   $( ".responsive-nav" ).show();
   });
   });
